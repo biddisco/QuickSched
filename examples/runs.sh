c66c256cf0608f3d5960575e827e8b4179e7ba7f
Pedro Gonnet <pedro.gonnet@durham.ac.uk>
2013-10-30 13:12:33 +0000

    add script for benchmarking.

---
+++ examples/runs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Executable and parameters
+prog = test_bh
+params = -n 1000000 -r 10
+
+# Main loop
+for n in {1..16}
+do
+
+    if [ ! -e ${prog}_${n}.dump ]
+    then
+        ./${prog} ${params} -t ${n} > ${prog}_${n}.dump
+    fi
+
+done
